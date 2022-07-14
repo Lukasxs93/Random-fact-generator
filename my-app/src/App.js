27a6801156d6b4e538d233a371fb96b3f0227306
Luca Porcelli <87008874+Lukasxs93@users.noreply.github.com>
2022-07-14 11:35:20 +0100

    fetch and button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,19 @@
-
+import Container from './components/Container';
+import{useState} from 'react';
 import './App.css';
 
 function App() {
+  const Styles = {
+    body:{
+      display:"flex",
+      flexDirection:"column",
+      justifyContent:"center",
+      alignContent:"center"
+    }
+  }
+   const[message, setMessage]=useState('Press the button to get a random fact !')
   return (
-    <div>hello</div>
+    <Container message={message} setMessage={setMessage}/>
   );
 }
 
--- my-app/src/components/Container.jsx
+++ my-app/src/components/Container.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Button from './Button'
 
-const Container =()=>{
+const Container =(props)=>{
+    function handleClick(){
+        getQuote();
+    }
+    const getQuote =()=>{
+        fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+        .then(response => response.json())
+        .then(data =>{props.setMessage(data.text);
+        console.log(data)});
+    }
     return(
         <div className="container" id="container">
-            
+            <h2>Did You know ?</h2>
+           <p>{props.message}</p>
+           <Button handleClick={handleClick}/>
         </div>
     );
 }
+
+export default Container;
